95a35de9c45904482288dc3027eb9b5e62f5709e
Marwin Carmo <marwinbr@gmail.com>
2023-10-06 12:08:27 -0700

    fix

--- colabs/barbara_mestrado/path_models.R
+++ colabs/barbara_mestrado/path_models.R
@@ -41,7 +41,7 @@ AD =~ ehad_1 + ehad_2 + ehad_3 + ehad_ 4 + ehad_5 + ehad_6 + ehad_7 +
 '
 fitHADSuni <- cfa(modHADSuni, data=neo, std.lv = TRUE, estimator = 'MLMV')
 
-summary(fitHADS, fit.measures = TRUE)
+summary(fitHADSuni, fit.measures = TRUE)
 
 ### Hierarchical model
 
@@ -74,4 +74,18 @@ summary(fitHADS, fit.measures = TRUE)
 # fitHADS    63 17426 17611 131.39                                  
 # fitHADSuni 77 17914 18038 647.49     405.97      14  < 2.2e-16 ***
 
+## Measurement model ----
 
+model <- '
+
+HADS =~ ehad_1 + ehad_2 + ehad_3 + ehad_ 4 + ehad_5 + ehad_6 + ehad_7 +
+    ehad_8 + ehad_9 + ehad_10 + ehad_11 + ehad_12 + ehad_13 + ehad_14
+
+Anx =~ ehad_1 + ehad_3 + ehad_5 + ehad_7 + ehad_9 + ehad_11 + ehad_13
+
+Dep =~ ehad_2 + ehad_4 + ehad_6 + ehad_8 + ehad_10 + ehad_12 + ehad_14
+
+
+
+
+'
